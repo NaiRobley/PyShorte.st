ef0b2254aeb999879c97867994b3e66b403f69d6
NaiRobley <robleyadrian@gmail.com>
2016-06-22 10:02:27 +0300

    Added Multiple URL Support

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # PyShst
 A short script to generate sh.st short links quickly
+
+Feel free to clone and contribute.
+
+Raise issues.
+
+If you have a Shorte.st account get your token from https://shorte.st/tools/api and replace it in the code.
--- shst.py
+++ shst.py
@@ -8,32 +8,32 @@ import sys
 
 __author__ = 'Robley Adrian < +254 722 161224 >'
 
-
-def main():
-	usage = """
+usage = """
 	Welcome to sh.st url shortener.
 
-	Usage: python shst.py [URL].
+	Usage: python shst.py URL1 URL2 URL3...
 
-	Example: python shst.py google.com
+	Example: python shst.py google.com twitter.com facebook.com
 
-	So far you can only shorten a single URL at a time.
-
-	Refer to the README at http:
+	More information at: http://github.com/NaiRobley/PyShorte.st
 
 
 	"""
-	try:
-		userUrl = sys.argv[1]
-		response = requests.put("https://api.shorte.st/v1/data/url", {"urlToShorten":userUrl}, headers={"public-api-token": "fee07e5aeff511b07b1dd67e06fddffe"})
-		shortened_url = json.loads(response.content)
-		print('')
-		print('Shortened URL: '+shortened_url['shortenedUrl'])
-		print('')
-		print('Status: '+shortened_url['status'])
 
-	except IndexError:
+def main():
+	userUrls = [url for url in sys.argv[1:]]
+	if userUrls > 0:
+		# userUrls = [url for url in sys.argv[1:]]
+		for userUrl in userUrls:
+			response = requests.put("https://api.shorte.st/v1/data/url", {"urlToShorten":userUrl}, headers={"public-api-token": "fee07e5aeff511b07b1dd67e06fddffe"})
+			shortened_url = json.loads(response.content)
+			print('')
+			print(userUrl+' : '+shortened_url['shortenedUrl']+' -- Status: '+shortened_url['status'])
+			print('')
+
+	elif userUrls == 0:
 		print (usage)
 
 if __name__ == '__main__':
+	print (usage)
 	main()
