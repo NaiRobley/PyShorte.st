ee615a5150730568322a09ac8cb2e5fe8b6e78f0
Robley Adrian <robleyadrian@gmail.com>
2017-01-15 14:51:45 +0300

    Added version number

--- shst.py
+++ shst.py
@@ -8,6 +8,7 @@ import os
 import sys
 
 __author__ = 'Robley Adrian < robleyadrian@gmail.com >'
+__version__ = '0.0.1'
 
 usage = """
 	Welcome to sh.st url shortener.
@@ -21,7 +22,7 @@ usage = """
 	"""
 
 def main():
-	userUrls = [url for url in sys.argv[1:]]
+	userUrls = [url for url in sys.argv[1:]] # Get the URLs from the user and add them to a list
 	try:
 		for userUrl in userUrls:
 			response = requests.put("https://api.shorte.st/v1/data/url", {"urlToShorten":userUrl}, headers={"public-api-token": "fee07e5aeff511b07b1dd67e06fddffe"})
